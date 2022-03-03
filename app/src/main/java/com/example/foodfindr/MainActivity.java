35708a78c4ac16d6b5ad6be2bd5f9d1d82f2297c
lar0041 <lar0041@mix.wvu.edu>
2022-03-03 15:52:24 -0500

    basic layout, needs to be filled in

--- app/src/main/java/com/example/foodfindr/MainActivity.java
+++ app/src/main/java/com/example/foodfindr/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.view.View;
 import android.os.Bundle;
 
+import com.example.foodfindr.ui.login.LoginActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         configureMapButton();
+        configureLoginButton();
+        configurePreferencesButton();
     }
 
     private void configureMapButton(){
@@ -24,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+    }
+
+    private void configureLoginButton(){
+        Button mapButton = (Button) findViewById(R.id.loginButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void configurePreferencesButton(){
+        Button mapButton = (Button) findViewById(R.id.preferencesButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, PreferencesActivity.class));
             }
         });
     }
---
+++ app/src/main/java/com/example/foodfindr/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.example.foodfindr.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.example.foodfindr.data.LoginDataSource;
+import com.example.foodfindr.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
