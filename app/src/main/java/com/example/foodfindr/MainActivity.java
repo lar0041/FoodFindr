ae08e475353d1dc668a1c1cb019afcaca1bef2f0
Abu Koroma <abkoroma@mail.wvu.edu>
2022-03-07 15:25:20 -0500

    Made changes to the MainActivity.java and AndroidManifest.xml. Added access to a newtwork to access database added a connection class with the database url. also addded required import files.

--- app/src/main/java/com/example/foodfindr/MainActivity.java
+++ app/src/main/java/com/example/foodfindr/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.foodfindr;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.StrictMode;
