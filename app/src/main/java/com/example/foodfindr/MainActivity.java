4b3288394b23d7b6cd0fd26b402026fee9123f67
Abu Koroma <abkoroma@mail.wvu.edu>
2022-03-07 13:38:08 -0500

    Made changes to the MainActivity.java and AndroidManifest.xml. Added access to a newtwork to access database added a connection class with the database url. also addded required import files.

--- app/src/main/java/com/example/foodfindr/MainActivity.java
+++ app/src/main/java/com/example/foodfindr/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.foodfindr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.StrictMode;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.os.Bundle;
 
 import com.example.foodfindr.ui.login.LoginActivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -50,5 +57,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, PreferencesActivity.class));
             }
         });
+    }
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnURL = "jdbc:jtds:sqlserver://foodfindr.database.windows.net:1433;DatabaseName=FoodFindr;user=FoodFindr@foodfindr;password={cs430Group6};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            conn = DriverManager.getConnection(ConnURL);
+        }
+        catch (SQLException se) {
+            Log.e("ERROR", se.getMessage());
+        }
+        catch (ClassNotFoundException e) {
+            Log.e("ERROR", e.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("ERROR", e.getMessage());
+        }
+        return conn;
     }
 }
