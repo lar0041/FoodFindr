46d82e4c50244d4408b5243ea8bc15282e00e4bb
msa0041 <89413281+msa0041@users.noreply.github.com>
2022-03-07 18:08:40 -0500

    Update

--- app/src/main/java/com/example/foodfindr/PreferencesActivity.java
+++ app/src/main/java/com/example/foodfindr/PreferencesActivity.java
@@ -2,7 +2,11 @@ package com.example.foodfindr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class PreferencesActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class PreferencesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+        Button button=(Button) findViewById(R.id.PreferenceBackButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PreferencesActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
