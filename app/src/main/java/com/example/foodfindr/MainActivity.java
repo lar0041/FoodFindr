a64dcfdef9f913c3606d5d795fcbbeba404a79c1
msa0041 <89413281+msa0041@users.noreply.github.com>
2022-04-04 18:54:05 -0400

    Mohammed Version Firebase

--- app/src/main/java/com/example/foodfindr/MainActivity.java
+++ app/src/main/java/com/example/foodfindr/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.foodfindr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.foodfindr.ui.login.LoginActivity;
 
@@ -22,10 +24,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context;
 
-        configureMapButton();
-        configureLoginButton();
-        configurePreferencesButton();
+
+        //configureMapButton();
+        //configureLoginButton();
+        //configurePreferencesButton();
+    }
+
+    private void show() {
     }
 
     private void configureMapButton(){
